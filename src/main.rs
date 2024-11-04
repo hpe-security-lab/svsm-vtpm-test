cf1087648837239e5cf7e16dfe1842c07ae7b4c1
Geoffrey Ndu <gtn@hpe.com>
2024-11-04 16:07:38 +0000

    Add code to verify Sha512(nonce||ekpub) from report

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use pretty_hex::*;
 use sev::firmware::guest::AttestationReport;
+use sha2::{Digest, Sha512};
 use std::fs;
 use tempfile::tempdir_in;
 
 use tss_esapi::{
-    abstraction::{ek, IntoKeyCustomization, KeyCustomization},
-    attributes::ObjectAttributesBuilder,
-    handles::{KeyHandle, NvIndexTpmHandle, TpmHandle},
+    abstraction::ek,
     interface_types::{
-        algorithm::{AsymmetricAlgorithm, HashingAlgorithm, PublicAlgorithm},
-        ecc::EccCurve,
-        key_bits::RsaKeyBits,
-        resource_handles::{Hierarchy, NvAuth},
-    },
-    structures::{
-        Digest, EccParameter, EccPoint, EccScheme, KeyDerivationFunctionScheme, Public,
-        PublicBuilder, PublicEccParametersBuilder, PublicKeyRsa, PublicRsaParametersBuilder,
-        RsaExponent, RsaScheme, SymmetricDefinitionObject,
+        algorithm::AsymmetricAlgorithm,
+        resource_handles::Hierarchy,
     },
     tcti_ldr::TctiNameConf,
     traits::Marshall,
@@ -24,7 +16,8 @@ use tss_esapi::{
 };
 
 fn main() {
-    println!("Hello, world!");
+
+    println!("Getting vTPM attestation report from SVSM uisng Linux configfs_tsm");
     //create temp directory
     let tmp_dir =
         tempdir_in("/sys/kernel/config/tsm/report").expect("Failed to create temp directory");
@@ -43,6 +36,10 @@ fn main() {
     //read outblob file
     let outblob = fs::read(tmp_dir.path().join("outblob")).expect("Failed to read outblob file");
     println!("outblob: {:?}", outblob.hex_dump());
+
+    //write outblob to file
+    fs::write("report.bin", &outblob).expect("Failed to write outblob to file");
+
     //read manifest file
     let manifest =
         fs::read(tmp_dir.path().join("manifestblob")).expect("Failed to read manifest file");
@@ -52,11 +49,19 @@ fn main() {
     let report: AttestationReport = bincode::deserialize(&outblob).unwrap();
     println!("report: {}", report);
 
+    println!("Verifying that the RSA 2048 EK public in the report matches one created in vTPM using TCG Profile");
+    println!("Creating EK public key using TCG Profile and comparing it to the one in the report");
+    println!("{}\n", "Using TSS 2.0 Enhanced System API Rust Wrapper, tss-esapi");
+
+
     //create ek using TCG Profile
     let algorithm = AsymmetricAlgorithm::Rsa;
     let ek_public = ek::create_ek_public_from_default_template(algorithm, None)
         .expect("Failed to create ek public key");
-    println!("ek_public_IN: {:?}", ek_public.marshall().unwrap().hex_dump());
+    println!(
+        "ek_public_IN: {:?}",
+        ek_public.marshall().unwrap().hex_dump()
+    );
     //Ensure that you set your TCTI environment variable before running this code
     // eg. export TPM2TOOLS_TCTI="device:/dev/tpmrm0" and if using sudo use sudo -E
     //To generate epub to compare against you can use tpm2_createek
@@ -69,15 +74,41 @@ fn main() {
             ctx.create_primary(Hierarchy::Endorsement, ek_public, None, None, None, None)
         })
         .expect("Failed to create ek");
-    let ekpub_tmpt_pub = ek.out_public
+    let ekpub_tmpt_pub = ek
+        .out_public
         .marshall()
         .expect("Failed to marshall ek public key");
-    
 
     println!("tmpt_public: {:?}", ekpub_tmpt_pub.hex_dump());
 
     //check that ek pub from tpm matches ek pub from manifest
     assert_eq!(ekpub_tmpt_pub, manifest);
+    println!("\n\n{}\n", "EK public key in the report matches the one created in vTPM!");
+
+    //recalculate Sha512(nonce||manifest)
+    println!("{}\n","Recalculating Sha512(nonce||ekpub) and verifying that it matches one in the report");
+    
+    //concatenate nonce and manifest
+    let hash_in = nonce.to_vec();
+    let hash_in = hash_in
+        .into_iter()
+        .chain(manifest.into_iter())
+        .collect::<Vec<u8>>();
+    println!("nonce||manifest: {:?}", hash_in.hex_dump());
+    let sha512 = Sha512::digest(&hash_in);
+    println!(
+        "Sha512(nonce||manifest): {:?}",
+        sha512.as_slice().hex_dump()
+    );
+
+    println!("report.report_data: {:?}", report.report_data.hex_dump());
+
+    println!("Sha512(nonce||manifest) matches one in the report.report_data");
+
+   
+
+    //verify that the hash matches the digest in the report
+    assert_eq!(sha512.as_slice(), report.report_data);
 
     //delete temp directory
     drop(tmp_dir);
